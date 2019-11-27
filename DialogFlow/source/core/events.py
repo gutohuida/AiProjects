0b786d531c79d03bc8821914ac9ee06e241fcae6
GUSTAVO HUIDA SA DOS SANTOS <GUSTAVO.SAN@uninter.com>
2019-11-27 15:59:42 -0300

    Mudança completa de mongo para MySQL no GoogleCloud

--- DialogFlow/source/core/events.py
+++ DialogFlow/source/core/events.py
@@ -3,31 +3,57 @@ from core.config import MONGO_CONFIG as CONFIG
 from datetime import date, datetime
 from unidecode import unidecode
 
+import dateutil.parser
+
+# def EventDate(EventName):
+#     db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
+#     documment = db.findOne({"Name":unidecode(EventName)})
+#     if documment["Date"]:
+#         date = datetime.strptime(documment["Date"],'%Y-%m-%d')
+#         return date.strftime('%d de %B'), documment["Time"]
+#     return None, None   
+
+# def EventLocation(EventName):
+#     db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
+#     documment = db.findOne({"Name":unidecode(EventName)})
+#     if documment["Location"]:
+#         return  documment["Location"]    
+#     return None
+
+# def Events():
+#     db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
+#     documments = db.findAll({})
+#     events = []
+#     for documment in documments:
+#         events.append(documment["Name"])
+#     return events
+
+# def EventDescription(EventName):
+#     db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")    
+#     documment = db.findOne({"Name":unidecode(EventName)})
+#     return documment["Description"]
 
 def EventDate(EventName):
-    db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
-    documment = db.findOne({"Name":unidecode(EventName)})
-    if documment["Date"]:
-        date = datetime.strptime(documment["Date"],'%Y-%m-%d')
-        return date.strftime('%d de %B'), documment["Time"]
+    documment = Evento.query.filter_by(evento_nome=EventName).first()
+    if documment.evento_data:
+        date = dateutil.parser.parse(str(documment.evento_data)).date()
+        time = dateutil.parser.parse(str(documment.evento_data)).time()
+        return date.strftime('%d de %B'), time
     return None, None   
 
 def EventLocation(EventName):
-    db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
-    documment = db.findOne({"Name":unidecode(EventName)})
-    if documment["Location"]:
-        return  documment["Location"]    
+    documment = Evento.query.filter_by(evento_nome=EventName).first()
+    if documment:
+        return  documment.evento_local
     return None
 
 def Events():
-    db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")
-    documments = db.findAll({})
+    documments = Evento.query.distinct(Evento.evento_nome)
     events = []
     for documment in documments:
-        events.append(documment["Name"])
+        events.append(documment.evento_nome)
     return events
 
 def EventDescription(EventName):
-    db = DbConector(CONFIG["host"],CONFIG["port"],CONFIG["db_name"],"Events")    
-    documment = db.findOne({"Name":unidecode(EventName)})
-    return documment["Description"]
+    documment = Evento.query.filter_by(evento_nome=EventName).first()
+    return documment.evento_desc
